3d82e944592c5c942181a2afb04a13c68ec4f3d5
legend <legend772196@gmail.com>
2025-09-19 20:40:12 +0300

    adding the screen and appbar

--- main.dart
+++ main.dart
@@ -1,3 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
 main(){
-  
+  runApp(MaterialApp(home: CyberToolApp(),));
 }
+
+class CyberToolApp extends StatelessWidget {
+  const CyberToolApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Cyber Security Tool"),
+          bottom: TabBar(
+           // controller: _tabController,
+            tabs: const [
+              Tab(text: "IP العام"),
+              Tab(text: "فحص DNS"),
+            ],
+          ),
+        )
+        }
+}
+
+
+
+
